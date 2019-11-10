52b902a8e5c4b5f97bb920c28326569c2f53ddd7
Theerawat Pongsupawat <rxseven.com@gmail.com>
2019-11-10 17:52:22 +0700

    Configure Storybook Backgrounds addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import { addDecorator, configure } from '@storybook/react';
+import { addParameters, addDecorator, configure } from '@storybook/react';
 
 import Wrapper from './decorators/Wrapper';
 import './styles.scss';
+
+// Backgrounds addon
+addParameters({
+  backgrounds: [
+    { default: true, name: 'Default', value: '#fff' },
+    { name: 'Dark', value: '#242424' },
+    { name: 'Facebook', value: '#3b5998' }
+  ]
+});
 
 // Import story files
 configure(require.context('../src', true, /\.stories\.jsx?$/), module);
