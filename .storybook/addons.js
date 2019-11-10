875a979928b220be84569070e97e75cae2eec6f0
Theerawat Pongsupawat <rxseven.com@gmail.com>
2019-11-10 17:41:57 +0700

    Register Storybook Storysource addon

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -3,4 +3,5 @@ import '@storybook/addon-actions/register';
 import '@storybook/addon-backgrounds/register';
 import '@storybook/addon-links/register';
 import '@storybook/addon-notes/register';
+import '@storybook/addon-storysource/register';
 import '@storybook/addon-viewport/register';
