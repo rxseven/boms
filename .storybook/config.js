62449862207e1bbed84b22e4d3ea7c4cf3a52598
Theerawat Pongsupawat <rxseven.com@gmail.com>
2019-11-10 17:28:45 +0700

    Configure global story wrapper

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,11 @@
+import React from 'react';
 import { addDecorator, configure } from '@storybook/react';
 
+import Wrapper from './decorators/Wrapper';
 import './styles.scss';
 
 // Import story files
 configure(require.context('../src', true, /\.stories\.jsx?$/), module);
+
+// Wrap each story within a container
+addDecorator(story => <Wrapper>{story()}</Wrapper>);
