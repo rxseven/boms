0921983cc9bfea542d3fa28fe2ab02178b8df610
Theerawat Pongsupawat <rxseven.com@gmail.com>
2019-11-15 15:26:40 +0700

    Add note templates for Storybook

--- src/stories/common/notes.js
+++ src/stories/common/notes.js
@@ -1,1 +1,6 @@
-export default {};
+export default {
+  hoc: 'An HOC returning children getting passed into a component.',
+  minimal:
+    'A minimal UI component returning children wrapping inside a container.',
+  skeleton: 'A skeleton component, no view layer.'
+};
