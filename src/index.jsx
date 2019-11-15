2d93fdc5237853f6986bd533650248c11ba9b3d0
Theerawat Pongsupawat <rxseven.com@gmail.com>
2019-11-15 18:39:33 +0700

    Refactor components structure

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import * as serviceWorker from 'serviceWorker';
-import App from 'components/App';
+import App from 'components/core/skeleton/App';
 import HTML from 'constants/common/elements';
 
 // Get root HTML element with the specified ID
